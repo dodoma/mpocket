15164fec94bd668523408a8b951321afed982c55
bigml <bigml@qq.com>
2024-09-25 09:30:22 +0800

    网络图片老是502，准备换成本地图片

--- lib/views/music_artist_screen.dart
+++ lib/views/music_artist_screen.dart
@@ -57,9 +57,9 @@ class _MusicArtistScreenState extends State<MusicArtistScreen> {
     return Scaffold(body: Center(child: CircularProgressIndicator()));
   } else {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.artist),
-      ),  
+      //appBar: AppBar(
+      //  title: Text(widget.artist),
+      //),  
       body: Center(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -67,11 +67,54 @@ class _MusicArtistScreenState extends State<MusicArtistScreen> {
             Container(
               width: containerWidth,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.network(meo.avt, height: 200,),
+                  Stack(
+                    children: [
+                      Image.network(
+                        meo.avt,
+                        width: MediaQuery.of(context).size.width,
+                        height: 220,
+                        fit: BoxFit.cover,
+                      ),
+                      Positioned(
+                        left: 10,  
+                        top: 20,
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(Icons.arrow_back, size: 32)
+                        )
+                      ),
+                      Positioned(
+                        right: 10,  
+                        bottom: 10,
+                        child: IconButton(
+                          onPressed: () {
+                            print('play shuffle');
+                          },
+                          icon: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey.withOpacity(0.7), // 灰色背景，带透明度
+                              shape: BoxShape.circle, // 圆形背景
+                            ),                            
+                            child: Icon(Icons.shuffle, size: 32, color: Colors.white,)
+                          )
+                        )
+                      )
+                    ]
+                  ),
+                  const Gap(10),
                   Row(
                     children: [
-                      Text('${meo.countAlbum} 张专辑 ${meo.countTrack} 首歌曲'),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('${widget.artist}', textScaler: TextScaler.linear(1.6),),
+                          Text('${meo.countAlbum} 张专辑 ${meo.countTrack} 首歌曲'),
+                        ],
+                      ),
                       Spacer(),
                       Icon(Icons.delete)
                     ],
@@ -86,14 +129,8 @@ class _MusicArtistScreenState extends State<MusicArtistScreen> {
                     child: LayoutBuilder(
                       builder: (BuildContext context, BoxConstraints constrains) {
                         return Container(
-                          padding: EdgeInsets.all(10),
+                          //padding: EdgeInsets.all(10),
                           height: constrains.maxHeight,
-                          decoration: BoxDecoration(
-                            //color: const Color.fromARGB(255, 21, 140, 236),
-                            color: Colors.grey[100],
-                            border: Border.all(color: Colors.grey[400]!),
-                            borderRadius: BorderRadius.circular(10), // 可选：圆角边框
-                          ),
                           child: ListView.builder(
                             itemCount: meo.albums.length,
                             itemBuilder: (context, index) {
@@ -101,7 +138,14 @@ class _MusicArtistScreenState extends State<MusicArtistScreen> {
                                 leading: Image.network(meo.albums[index].cover, height: 60),
                                 title: Text(meo.albums[index].name),
                                 subtitle: Text('${meo.albums[index].PD} ${meo.albums[index].countTrack} 首'),
-                                trailing: Icon(Icons.play_arrow),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    if (meo.albums[index].cached) Text('已缓存'),
+                                    IconButton(onPressed: () {print('xxxx');}, icon: Icon(size: 20, Icons.play_arrow)),
+                                    IconButton(onPressed: () {print('yyyy');}, icon: Icon(size: 20, Icons.more_vert)),
+                                  ],
+                                )
                               );
                             }
                           )
